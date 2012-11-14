408d4ec0bb4d925dc5db27a5e0e618f69cbb440a
pocketberserker <pocketberserker@gmail.com>
2012-11-15 04:52:24 +0900

    15の倍数についても一気に実装

--- src/src/java/tdd/pragmatic/FizzBuzz.java
+++ src/src/java/tdd/pragmatic/FizzBuzz.java
@@ -2,7 +2,8 @@ package tdd.pragmatic;
 
 public class FizzBuzz {
     public String say(int number) {
-        if(number % 5 == 0) return "Buzz";
-        return "Fizz";
+        if(number % 5 == 0 && number % 3 == 0) return "FizzBuzz";
+        else if(number % 5 == 0) return "Buzz";
+        else return "Fizz";
     }
 }

07210e74832f865a7120bd47d327c1e6ff44d971
pocketberserker <pocketberserker@gmail.com>
2012-11-15 04:28:54 +0900

    期待値から記述するassertファースト

---
+++ src/test/java/tdd/pragmatic/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package tdd.pragmatic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void 数値3を渡すとFizzが返ってくる() throws Exception {
+        assertThat(fizzbuzz.say(3),is("Fizz"));
+    }
+}

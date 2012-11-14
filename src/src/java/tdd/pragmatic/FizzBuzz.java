6af7006a6a82256c6bbf4aa0f9781f37a1f0070a
pocketberserker <pocketberserker@gmail.com>
2012-11-15 04:55:56 +0900

    5の倍数の判定部分をprivateメソッドの呼び出しに置き換え

--- src/src/java/tdd/pragmatic/FizzBuzz.java
+++ src/src/java/tdd/pragmatic/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
     }
 
     public String say(int number) {
-        if(number % 5 == 0 && number % 3 == 0) return "FizzBuzz";
+        if(isMultipleOfFive(number) && number % 3 == 0) return "FizzBuzz";
         else if(number % 5 == 0) return "Buzz";
         else return "Fizz";
     }

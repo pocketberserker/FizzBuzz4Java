9071ec86af18a60538dc20825368f1c4ab35b475
pocketberserker <pocketberserker@gmail.com>
2012-11-15 04:29:21 +0900

    変数がないと怒られるので、IDEの力を借りて変数を自動生成

--- src/test/java/tdd/pragmatic/FizzBuzzTest.java
+++ src/test/java/tdd/pragmatic/FizzBuzzTest.java
@@ -8,6 +8,7 @@ public class FizzBuzzTest {
 
     @Test
     public void 数値3を渡すとFizzが返ってくる() throws Exception {
+        FizzBuzz fizzbuzz = null;
         assertThat(fizzbuzz.say(3),is("Fizz"));
     }
 }
